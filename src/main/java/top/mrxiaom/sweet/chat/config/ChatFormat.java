87ce2f5761ec982127333f12c1db16ccf732eac1
MrXiaoM <coolxiaom95@gmail.com>
2026-01-29 20:17:52 +0800

    add chat format config

---
+++ src/main/java/top/mrxiaom/sweet/chat/config/ChatFormat.java
@@ -0,0 +1,69 @@
+package top.mrxiaom.sweet.chat.config;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.permissions.Permissible;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import top.mrxiaom.pluginbase.utils.ConfigUtils;
+import top.mrxiaom.sweet.chat.api.ChatContext;
+import top.mrxiaom.sweet.chat.api.IFormatPart;
+import top.mrxiaom.sweet.chat.func.ChatListener;
+
+import java.util.List;
+
+public class ChatFormat {
+    private final @NotNull String id;
+    private final @Nullable String permission;
+    private final int priority;
+    private final @NotNull List<IFormatPart> chatFormat;
+    private final @NotNull List<IFormatPart> crossServerFormat;
+    @ApiStatus.Internal
+    public ChatFormat(@NotNull ChatListener parent, @NotNull String id, ConfigurationSection config) throws Exception {
+        this.id = id;
+        String permission = config.getString("permission", "none");
+        if (permission.equals("none")) {
+            this.permission = null;
+        } else {
+            this.permission = permission;
+        }
+        this.priority = config.getInt("priority", 1000);
+        this.chatFormat = parent.loadFormat(ConfigUtils.getSectionList(config, "chat-format"));
+        if (config.contains("cross-server-format")) {
+            this.crossServerFormat = parent.loadFormat(ConfigUtils.getSectionList(config, "cross-server-format"));
+        } else {
+            this.crossServerFormat = chatFormat;
+        }
+    }
+
+    @NotNull
+    public String id() {
+        return id;
+    }
+
+    public boolean hasPermission(@NotNull Permissible p) {
+        return permission == null || p.hasPermission(permission);
+    }
+
+    public int priority() {
+        return priority;
+    }
+
+    public TextComponent.Builder build(ChatContext ctx) {
+        return build(chatFormat, ctx);
+    }
+
+    public TextComponent.Builder buildCrossServer(ChatContext ctx) {
+        return build(crossServerFormat, ctx);
+    }
+
+    private static TextComponent.Builder build(List<IFormatPart> partList, ChatContext ctx) {
+        TextComponent.Builder builder = Component.text();
+        for (IFormatPart part : partList) {
+            builder.append(part.get(ctx));
+        }
+        return builder;
+    }
+}
--- src/main/java/top/mrxiaom/sweet/chat/func/ChatListener.java
+++ src/main/java/top/mrxiaom/sweet/chat/func/ChatListener.java
@@ -1,29 +1,37 @@
 package top.mrxiaom.sweet.chat.func;
 
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.pluginbase.func.AutoRegister;
+import top.mrxiaom.pluginbase.utils.AdventureUtil;
+import top.mrxiaom.pluginbase.utils.ConfigUtils;
 import top.mrxiaom.pluginbase.utils.Util;
 import top.mrxiaom.sweet.chat.SweetChat;
 import top.mrxiaom.sweet.chat.api.ChatContext;
 import top.mrxiaom.sweet.chat.api.IFormatPart;
 import top.mrxiaom.sweet.chat.api.IFormatPartProvider;
+import top.mrxiaom.sweet.chat.config.ChatFormat;
 import top.mrxiaom.sweet.chat.impl.format.PartPlain;
 import top.mrxiaom.sweet.chat.impl.format.PartPlayerMessage;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.io.File;
+import java.util.*;
 
 @AutoRegister
 public class ChatListener extends AbstractModule implements Listener {
     private final Map<String, IFormatPartProvider> partRegistry = new HashMap<>();
+    private final Map<String, ChatFormat> chatFormatMap = new HashMap<>();
+    private final List<ChatFormat> chatFormatWithPriority = new ArrayList<>();
     public ChatListener(SweetChat plugin) {
         super(plugin);
         registerPart("plain", PartPlain::new);
@@ -45,11 +53,57 @@ public class ChatListener extends AbstractModule implements Listener {
         }, plugin, false);
     }
 
+    private void reloadChatFormat(File folder) {
+        chatFormatMap.clear();
+        chatFormatWithPriority.clear();
+        Util.reloadFolder(folder, false, (id, file) -> {
+            YamlConfiguration config = ConfigUtils.load(file);
+            try {
+                ChatFormat format = new ChatFormat(this, id, config);
+                chatFormatMap.put(id, format);
+                chatFormatWithPriority.add(format);
+            } catch (Throwable t) {
+                warn("[chat] 加载聊天格式配置 " + id + " 时出现错误: " + t.getMessage());
+            }
+        });
+        chatFormatWithPriority.sort(Comparator.comparingInt(ChatFormat::priority));
+        info("[chat] 加载了 " + chatFormatMap.size() + " 个聊天格式配置");
+    }
+
     private boolean onChat(Player player, String text) {
         ChatContext ctx = new ChatContext(plugin, player, text);
+        // TODO 实现全局模式、本地模式
+        ChatFormat format = getChatFormat(chatFormatWithPriority, player);
+        if (format == null) {
+            return false;
+        }
 
-        // TODO: 处理聊天消息
-        return false;
+        TextComponent component = format.build(ctx).build();
+        AdventureUtil.sendMessage(Bukkit.getConsoleSender(), component);
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            AdventureUtil.sendMessage(p, component);
+        }
+
+        return true;
+    }
+
+    @Nullable
+    public ChatFormat getChatFormat(List<ChatFormat> list, Player player) {
+        for (ChatFormat format : list) {
+            if (format.hasPermission(player)) {
+                return format;
+            }
+        }
+        return null;
+    }
+
+    @NotNull
+    public List<IFormatPart> loadFormat(@NotNull List<ConfigurationSection> configList) throws Exception {
+        List<IFormatPart> list = new ArrayList<>();
+        for (ConfigurationSection section : configList) {
+            list.add(loadFormat(section));
+        }
+        return list;
     }
 
     /**
@@ -57,7 +111,7 @@ public class ChatListener extends AbstractModule implements Listener {
      * @param config 配置 section
      */
     @NotNull
-    public IFormatPart load(@NotNull ConfigurationSection config) throws Exception {
+    public IFormatPart loadFormat(@NotNull ConfigurationSection config) throws Exception {
         String type = config.getString("type");
         if (type == null) {
             throw new IllegalArgumentException("未输入参数 type");
