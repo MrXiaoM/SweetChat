144861cd99315df8e02654240b12a3fb631402ec
MrXiaoM <coolxiaom95@gmail.com>
2026-01-30 14:09:22 +0800

    add regex support

--- src/main/java/top/mrxiaom/sweet/chat/impl/mode/StringMatchUtils.java
+++ src/main/java/top/mrxiaom/sweet/chat/impl/mode/StringMatchUtils.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringMatchUtils {
     @NotNull
@@ -20,6 +22,18 @@ public class StringMatchUtils {
         if (input.endsWith("*")) {
             String prefix = input.substring(0, input.length() - 1);
             return str -> str.startsWith(prefix);
+        }
+        if (input.startsWith("$ ")) {
+            try {
+                Pattern regex = Pattern.compile(input.substring(2));
+                return str -> {
+                    Matcher matcher = regex.matcher(str);
+                    int length = str.length();
+                    return matcher.matches() && matcher.start() == 0 && matcher.end() == length;
+                };
+            } catch (IllegalArgumentException e) {
+                return str -> false;
+            }
         }
         return str -> str.equals(input);
     }
